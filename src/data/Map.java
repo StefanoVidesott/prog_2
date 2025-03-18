92f88e20e726fa2c62951117680632f329ba86f8
Stefano Videsott <ste.videsott@gmail.com>
2025-03-18 15:13:08 +0100

    Map implementation

--- src/data/Map.java
+++ src/data/Map.java
@@ -1,5 +1,31 @@
 package data;
 
-public class Map {
+import data.Block;
 
+public class Map {
+    private Block[][] content;
+    private int  width = 15;
+    private int height = 15;
+
+    public Map() {
+        this.content =  new Block[width][height];
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                this.content[i][j] = new Block();
+            }
+        }
+    }
+
+    public void display_on_out() {
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                System.out.print(this.content[i][j].toString());
+            }
+            System.out.println();
+        }
+    }
+
+    public void change_cell(int row, int col) {
+        this.content[row][col] = new Block('A');
+    }
 }
