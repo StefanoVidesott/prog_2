4b0dae4d6cf42279dec49d79180a4b88410679b4
Stefano Videsott <ste.videsott@gmail.com>
2025-03-18 15:31:09 +0100

    main map implementation

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,28 @@
 package Main;
 
 import data.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Map mappa = new Map(5, 5);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Mappa iniziale:");
+        mappa.displayOnOut();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Inserisci riga: ");
+            int row = scanner.nextInt();
+
+            System.out.print("Inserisci colonna: ");
+            int col = scanner.nextInt();
+
+            mappa.changeCell(row, col);
+            mappa.displayOnOut();
+        }
+
+        scanner.close();
     }
 }
--- src/data/Map.java
+++ src/data/Map.java
@@ -4,28 +4,31 @@ import data.Block;
 
 public class Map {
     private Block[][] content;
-    private int  width = 15;
-    private int height = 15;
+    private int  width;
+    private int height;
 
-    public Map() {
-        this.content =  new Block[width][height];
-        for(int i = 0; i < width; i++) {
-            for(int j = 0; j < height; j++) {
+    public Map(int _width, int  _height) {
+        this.width = _width;
+        this.height = _height;
+
+        this.content =  new Block[_width][_height];
+        for(int i = 0; i < _width; i++) {
+            for(int j = 0; j < _height; j++) {
                 this.content[i][j] = new Block();
             }
         }
     }
 
-    public void display_on_out() {
+    public void displayOnOut() {
         for(int i = 0; i < width; i++) {
             for(int j = 0; j < height; j++) {
-                System.out.print(this.content[i][j].toString());
+                System.out.print(this.content[i][j].display());
             }
             System.out.println();
         }
     }
 
-    public void change_cell(int row, int col) {
+    public void changeCell(int row, int col) {
         this.content[row][col] = new Block('A');
     }
 }
